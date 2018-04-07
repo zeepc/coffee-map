658b0da5eb7595d0443e3899658553e9b7b27637
zee <zperezcanals@gmail.com>
2018-04-06 20:00:26 -0400

    remove materialize and add FiDi lat,long coordinates

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
   var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 15
+          center: {lat: 40.707, lng: -74.006},
+          zoom: 16
         });
       }
