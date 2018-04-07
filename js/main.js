0be4d02c776966ec16b38e96f8929273e2d7e74b
zee <zperezcanals@gmail.com>
2018-04-06 20:42:42 -0400

    add map styling choices for user

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
- 
 
-// // Call map on location to display on page
-// let map;
-// let initMap = () => {
-//     map = new google.maps.Map(document.getElementById('map'), 
-//     {center: {lat: -34.397, lng: 150.644}, zoom: 8
-//     });
-// }
   var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 40.707, lng: -74.006},
-          zoom: 16
+          zoom: 15,
+          mapTypeControl:false
+        });
+// Add a style-selector control to the map.
+      var styleControl = document.getElementById('style-selector-control');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(styleControl);
+
+        // Set the map's style to the initial value of the selector.
+      var styleSelector = document.getElementById('style-selector');
+        map.setOptions({styles: styles[styleSelector.value]});
+
+        // Apply new JSON when the user selects a different style.
+        styleSelector.addEventListener('change', function() {
+          map.setOptions({styles: styles[styleSelector.value]});
         });
       }
